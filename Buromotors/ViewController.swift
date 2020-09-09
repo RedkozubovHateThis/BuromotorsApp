347b3466163f4b9cfd90dbac719427002c648495
Антон Редкозубов <uacreez@gmail.com>
2020-09-09 12:09:38 +0400

    Initial Commit

---
+++ Buromotors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buromotors
+//
+//  Created by Anton Redkozubov on 09.09.2020.
+//  Copyright © 2020 Anton Redkozubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
